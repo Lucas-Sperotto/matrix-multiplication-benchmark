e7e70622325399078248ef6f4dbe6c4b232754ec
Lucas Kriesel Sperotto <sperotto.lucas@outlook.com>
2025-09-03 11:44:53 -0300

    Alterado mensagem em printf e comentários.

--- src/matriz_c.c
+++ src/matriz_c.c
@@ -46,19 +46,18 @@ void multiply(int **mat1, int **mat2, int **res, int N)
 
 int main()
 {
-     FILE *f = fopen("resultado_c.csv", "w");
-        if (f == NULL)
-        {
-            printf("Erro ao abrir o arquivo!\n");
-            return 1;
-        }
+    FILE *f = fopen("resultado_c.csv", "w");
+    if (f == NULL)
+    {
+        printf("Erro ao abrir o arquivo!\n");
+        return 1;
+    }
 
-    fprintf(f, "N,TCS,TAM,TLM\n");//
+    fprintf(f, "N,TCS,TAM,TLM\n"); //
 
+    // Varie N automaticamente de 10 a 1000
     for (int N = 10; N <= 1000; N)
-    { // Varie N automaticamente de 10 a 10000
-
-       
+    {
 
         // Medindo o tempo de alocação de memória
         clock_t start_alloc = clock();
@@ -127,11 +126,10 @@ int main()
         double time_free = ((double)(end_free - start_free)) / CLOCKS_PER_SEC;
 
         // Salvando os resultados no arquivo
-        
-        fprintf(f, "%d,", N);// valor de N
-        fprintf(f, "%E,", time_calc);//Tempo de cálculo: %f segundos\n
-        fprintf(f, "%E,", time_alloc); //Tempo de alocação de memória: %f segundos\n        
-        fprintf(f, "%E\n", time_free);//Tempo de liberação de memória: %f segundos
+        fprintf(f, "%d,", N);          // valor de N
+        fprintf(f, "%E,", time_calc);  // Tempo de cálculo: %f segundos\n
+        fprintf(f, "%E,", time_alloc); // Tempo de alocação de memória: %f segundos\n
+        fprintf(f, "%E\n", time_free); // Tempo de liberação de memória: %f segundos
         // fprintf(f, "Memória usada: %ld KB\n", memory_used_kb);
 
         printf("Resultados para N = %d salvos.\n", N);
@@ -141,9 +139,8 @@ int main()
             N += 100;
         else
             N += 100;
-        
     }
     fclose(f);
-    printf("Todos os resultados foram salvos no arquivo resultado_c.dat.\n");
+    printf("Todos os resultados foram salvos no arquivo resultado_c.csv.\n");
     return 0;
 }
