9561f92bc385e1ed29509e2339719fcb4bbb654f
Marcos Adriano <Marcos Adriano>
2025-09-10 15:16:39 -0300

    Codificado o calculo da media para o valor de M repetições do codigo utilizando o args para capturar o valor de de M repetições

--- src/MatrixMultiplication.java
+++ src/MatrixMultiplication.java
@@ -46,49 +46,64 @@ public class MatrixMultiplication {
 
             writer.write("N,TCS,TAM\n");
 
-            for (int N : new int[] { 10, 100, 500, 1000, 1500, 2000, 2500, 3000, }) { // Varie N automaticamente
+            int M = 1; // valor padrão
 
-                // Tempo de alocação de memória
-                long startAlloc = System.nanoTime();
-                int[][] mat1 = new int[N][N];
-                int[][] mat2 = new int[N][N];
-                int[][] res = new int[N][N];
+            if (args.length == 1) {
+                M = Integer.parseInt(args[0]); // converte string para int
+            }
 
-                // Inicializando as matrizes
-                for (i = 0; i < N; i++) {
-                    for (j = 0; j < N; j++) {
-                        mat1[i][j] = i + j;
-                        if (i == j)
-                            mat2[i][j] = 1;
-                        else {
-                            mat2[i][j] = 0;
+            System.out.println(" M: " + M);
+
+            for (int N : new int[] { 10, 100, 500, 1000}) { // Varie N automaticamente
+
+                double timeAlloc = 0.0, timeCalc = 0.0;
+
+                for (int m = 1; m <= M; m++) {
+                    
+                    // Tempo de alocação de memória
+                    long startAlloc = System.nanoTime();
+                    int[][] mat1 = new int[N][N];
+                    int[][] mat2 = new int[N][N];
+                    int[][] res = new int[N][N];
+
+                    // Inicializando as matrizes
+                    for (i = 0; i < N; i++) {
+                        for (j = 0; j < N; j++) {
+                            mat1[i][j] = i + j;
+                            if (i == j)
+                                mat2[i][j] = 1;
+                            else {
+                                mat2[i][j] = 0;
+                            }
                         }
                     }
-                }
-                long endAlloc = System.nanoTime();
-                double timeAlloc = (endAlloc - startAlloc) / 1e9;
+                    long endAlloc = System.nanoTime();
+                    timeAlloc += (endAlloc - startAlloc) / 1e9;
 
-                // Tempo do cálculo
-                long startCalc = System.nanoTime();
-                multiply(mat1, mat2, res, N);
-                long endCalc = System.nanoTime();
-                double timeCalc = (endCalc - startCalc) / 1e9;
+                    // Tempo do cálculo
+                    long startCalc = System.nanoTime();
+                    multiply(mat1, mat2, res, N);
+                    long endCalc = System.nanoTime();
+                    timeCalc += (endCalc - startCalc) / 1e9;
 
-                // Java faz a coleta de lixo automaticamente, então não medimos o tempo de
-                // liberação manual.
+                    // Java faz a coleta de lixo automaticamente, então não medimos o tempo de
+                    // liberação manual.
 
-                // Verificação do resultado
-                for (i = 0; i < N; i++) {
-                    for (j = 0; j < N; j++) {
-                        if (res[i][j] != i + j)
-                            System.out.println("Erro na multiplicação das matrizes para N = " + N + "!");
+                    // Verificação do resultado
+                    for (i = 0; i < N; i++) {
+                        for (j = 0; j < N; j++) {
+                            if (res[i][j] != i + j)
+                                System.out.println("Erro na multiplicação das matrizes para N = " + N + "!");
+                        }
                     }
+                    System.out.println(N + ",");
+                    System.out.println(String.format("%.6e", timeCalc) + ",");
+                    System.out.println(String.format("%.6e", timeAlloc) + "\n");
                 }
-
                 // Salvando os resultados no arquivo
                 writer.write(N + ",");
-                writer.write(String.format("%.6e", timeCalc) + ",");
-                writer.write(String.format("%.6e", timeAlloc) + "\n");
+                writer.write(String.format("%.6e", (timeCalc/M)) + ",");
+                writer.write(String.format("%.6e", (timeAlloc/M)) + "\n");
 
                 System.out.println("Resultados para N = " + N + " salvos.");
 
